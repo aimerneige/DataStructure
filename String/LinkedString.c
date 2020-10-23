38b93324aeb6593504762a7ee2ab8bcf5858b11e
AimerNeige <aimer.neige.soft@gmail.com>
2020-10-23 16:48:32 +0800

    update

--- String/LinkedString.c
+++ String/LinkedString.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 #define MAXSIZE 100
 
@@ -16,8 +18,34 @@ typedef struct _string
 // replase 替换
 // search 查找
 
+String *str_init(char *src);
+
 int main(int argc, char const *argv[])
 {
 
     return 0;
+}
+
+String *str_init(char *src)
+{
+    if (src == NULL)
+    {
+        return NULL;
+    }
+    int len = strlen(src);
+    if (len > MAXSIZE)
+    {
+        printf("The string is too long!\n");
+    }
+    String *link = (String *)malloc(sizeof(String) * 1);
+    String *head = link;
+    link->next = NULL;
+    for (int i = 0; i < len; i++)
+    {
+        link->data = src[i];
+        link->next = (String *)malloc(sizeof(String) * 1);
+        link = link->next;
+    }
+    link->next = NULL;
+    return head;
 }
